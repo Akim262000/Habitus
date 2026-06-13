c83d8fb32c4eba605dd512c086f87b3825d3be14
Akim Minko <akim.minko@yandex.ru>
2026-06-13 18:43:48 +0300

    feat: add section look-inside

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 document.querySelector(".navbar-toggler").addEventListener("click", function () {
   this.classList.toggle("open");
 });
+
+$(".look-inside-slider").slick({
+  infinite: true,
+  autoplay: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  speed: 300,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 767,
+      settings: {
+        arrows: false,
+        slidesToShow: 2,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+                arrows: false,
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
