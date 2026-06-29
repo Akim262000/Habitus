4fef7f7d7ffd80ad64114326d0d032fdb853f599
Akim Minko <akim.minko@yandex.ru>
2026-06-29 12:16:01 +0300

    fix: refactor burger-menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
-document.querySelector(".navbar-toggler").addEventListener("click", function () {
+const toggler = document.querySelector(".navbar-toggler");
+const overlay = document.querySelector(".overlay");
+
+toggler.addEventListener("click", function () {
   this.classList.toggle("open");
+  overlay.classList.toggle("show");
 });
+
 
 $(".look-inside-slider").slick({
   infinite: true,
